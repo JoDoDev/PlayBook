d0b50b26fab2f9ae7c016e0bb2f73595bf3b175e
Donato Wolfisberg <donatowolfisberg@gmail.com>
2017-04-13 11:19:00 +0200

    created login method on Database Helper

---
+++ PlayBookServer/src/database/DatabaseHelper.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const databaseConnectionProvider = require("./DatabaseConnectionProvider");
+
+let DatabaseHelper = class DatabaseHelper {
+  async login(username, password) {
+    try {
+      let dbConn = await databaseConnectionProvider.getConnection();
+      let sqlPrepare = 'SELECT ( CASE WHEN COUNT(*) = 1 THEN "true" ELSE "false" END) AS "sucsess" from `user` WHERE `email` = ? AND `password` = ? OR `username` = ? AND `password` = ?';
+      let inserts = [username, password, username, password];
+      let results = await this.makeQuery(sqlPrepare, inserts, dbConn);
+      return results;
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  makeQuery(sqlPrepare, inserts, dbConn) {
+    return (new Promise((fulfill, reject) => {
+        dbConn.query(sqlPrepare, inserts, (err, results, fields) => {
+        if(err) {
+          reject(error);
+        } else {
+          fulfill(results);
+        }
+        });
+      })
+    );
+  }
+
+};
+
+
+let databaseHelper = new DatabaseHelper();
+module.exports = databaseHelper;
